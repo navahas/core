a4d4cca850d1eac17889f27557b94ba7db4afb94
Carlos Navajas <c.navajas4@gmail.com>
2025-03-22 18:01:51 +0100

    ..

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -21,13 +21,11 @@ private:
 
     // Procedure: Increment the counter by 1
     PUBLIC_PROCEDURE(Increment)
-        qpi.log("Incrementing counter !");
         state.counter++;
     _
 
     // Function: Return the current counter value
     PUBLIC_FUNCTION(GetCounter)
-        qpi.log("Getting the counter state!");
         output.value = state.counter;
     _
 
