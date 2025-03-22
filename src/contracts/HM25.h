9a10e1fdceba44f6b1fbf0d88cc352c8edd83d79
Carlos Navajas <c.navajas4@gmail.com>
2025-03-22 17:54:27 +0100

    ..attempt to update contract

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -21,11 +21,13 @@ private:
 
     // Procedure: Increment the counter by 1
     PUBLIC_PROCEDURE(Increment)
+        qpi.log("Incrementing counter !");
         state.counter++;
     _
 
     // Function: Return the current counter value
     PUBLIC_FUNCTION(GetCounter)
+        qpi.log("Getting the counter state!");
         output.value = state.counter;
     _
 
