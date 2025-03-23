52a1e8d4f863ceb0561fa3755b65aa1e81ebd650
Carlos Navajas <c.navajas4@gmail.com>
2025-03-23 09:39:31 +0100

    return to counter

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -1,63 +1,42 @@
 using namespace QPI;
-struct TrustScoreCounter : public ContractBase
+
+struct HM252
+{
+};
+
+struct HM25 : public ContractBase
 {
 public:
-    struct IncrementPayment_input {
-        identity tenant;
-    };
-    struct GetTrustScore_input {
-        identity tenant;
-    };
-    struct GetTrustScore_output {
-        uint8 score;
-        uint64 totalPayments;
-        uint64 successfulPayments;
-    };
-    struct Init_input {
-        identity admin;
-    };
-private:
-    identity admin;
-    struct TenantInfo {
-        uint64 totalPayments;
-        uint64 successfulPayments;
-    };
-    map<identity, TenantInfo> tenants;
+    struct Increment_input {};
+    struct Increment_output {};
 
-    // Initialize with admin
-    PUBLIC_PROCEDURE(Init)
-        assert(msg.sender != identity::null());
-        admin = input.admin;
+    struct GetCounter_input {};
+    struct GetCounter_output
+    {
+        uint64 value;
+    };
+
+private:
+    uint64 counter;
+
+    // Procedure: Increment the counter by 1
+    PUBLIC_PROCEDURE(Increment)
+        state.counter++;
     _
-    
-    // Increment successful payments for a tenant
-    PUBLIC_PROCEDURE(IncrementPayment)
-        assert(msg.sender == admin);
-        auto& t = tenants[input.tenant];
-        t.totalPayments++;
-        t.successfulPayments++;
+
+    // Function: Return the current counter value
+    PUBLIC_FUNCTION(GetCounter)
+        output.value = state.counter;
     _
-    
-    // Get the current trust score for a tenant
-    PUBLIC_FUNCTION(GetTrustScore)
-        auto& t = tenants[input.tenant];
-        if (t.totalPayments > 0)
-            output.score = (t.successfulPayments * 10) / t.totalPayments;
-        else
-            output.score = 0;
-        output.totalPayments = t.totalPayments;
-        output.successfulPayments = t.successfulPayments;
-    _
-    
+
     // Register function and procedure with IDs
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
-        REGISTER_USER_PROCEDURE(Init, 1);
-        REGISTER_USER_PROCEDURE(IncrementPayment, 2);
-        REGISTER_USER_FUNCTION(GetTrustScore, 1);
+        REGISTER_USER_PROCEDURE(Increment, 1);
+        REGISTER_USER_FUNCTION(GetCounter, 1);
     _
-    
+
     // Initial state setup
     INITIALIZE
-        admin = identity::null();
+        state.counter = 19;
     _
 };
